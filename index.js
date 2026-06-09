359e9df2a7d6e6bbc5ab33d8b1e9430a8bf2c61b
nithinK-142 <nithin.sagar.359@gmail.com>
2026-06-09 09:53:33 +0530

    add: ws server

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-const http = require('http');
+const http = require('node:http');
+const { WebSocketServer } = require('ws')
+const fs = require('node:fs/promises')
+const path = require('node:path')
 
 const PORT = process.env.PORT ?? 8000;
 
-const httpServer = http.createServer()
+const httpServer = http.createServer(async (req, res) => {
+    const htmlFile = await fs.readFile(path.resolve('./index.html'), 'utf-8')
+    res.setHeader('Content-Type', 'text/html')
+    return res.end(htmlFile)
+})
+
+const wsServer = new WebSocketServer({ server: httpServer })
+
+wsServer.on('connection', (ws) => {
+    console.log(ws);
+    console.log("websocket connection established")
+})
 
 httpServer.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
